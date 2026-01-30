6973d6d25ff30cc0c950b5af390ef103b327e323
Thoxie <Paul@innitstreetwear.com>
2026-01-30 12:07:15 -0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,13 @@
-// tailwind.config.ts
+// PATH: tailwind.config.ts
 
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}",
-  ],
+  content: ["./app/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [],
 };
 
 export default config;
-
