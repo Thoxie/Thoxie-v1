0d0b92904008d32bcfae17b8d2986bf5720a5b51
Thoxie <Paul@innitstreetwear.com>
2026-01-31 12:25:50 -0500

    Integrate TailwindCSS and remove postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   plugins: {
+    tailwindcss: {}, // TailwindCSS integration
     autoprefixer: {}, // Adds vendor prefixes automatically
-    'postcss-preset-env': {} // Enables modern CSS features as needed
   },
 };
