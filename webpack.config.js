a5211a0f22f7ec0cdae8cbdc70b8d9dd48a4f0d6
Thoxie <Paul@innitstreetwear.com>
2026-02-02 08:51:17 -0500

    Enhance comments in webpack.config.js
    Updated comments in webpack configuration for clarity.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,24 @@
-module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.(css|scss|sass)$/,
-                use: [
-                    'style-loader',
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            postcssOptions: {
-                                plugins: [
-                                    // Add PostCSS plugins here
-                                    require('autoprefixer'),
-                                    require('cssnano')
-                                ],
-                            },
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-};
++module.exports = {
++    module: {
++        rules: [
++            {
++                test: /\.(css|scss|sass)$/,  // Matches .css, .scss, and .sass files
++                use: [
++                    'style-loader',          // Injects styles into the DOM
++                    'css-loader',            // Enables importing CSS files
++                    {
++                        loader: 'postcss-loader',
++                        options: {
++                            postcssOptions: {
++                                plugins: [
++                                    require('autoprefixer'), // Adds vendor prefixes
++                                    require('cssnano')       // Minifies CSS
++                                ],
++                            },
++                        },
++                    },
++                ],
++            },
++        ],
++    },
++};
