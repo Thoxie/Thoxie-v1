149bcb71bbce90ada92cab5616bd864a71bd1e38
Thoxie <Paul@innitstreetwear.com>
2026-02-07 13:54:37 -0500

    Add PageTitle component for page headings

--- app/_components/PageTitle.js
+++ app/_components/PageTitle.js
@@ -1,1 +1,16 @@
+// path: /app/_components/PageTitle.js
+export default function PageTitle({ children }) {
+  return (
+    <h1
+      style={{
+        marginTop: 0,
+        marginBottom: "14px",
+        fontSize: "28px",
+        fontWeight: 900,
+      }}
+    >
+      {children}
+    </h1>
+  );
+}
 
