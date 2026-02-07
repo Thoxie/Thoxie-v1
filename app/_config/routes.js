a58435d7e80a613bd4908a9df2fe543189b83772
Thoxie <Paul@innitstreetwear.com>
2026-02-07 12:34:04 -0500

    Define application routes in routes.js

--- app/_config/routes.js
+++ app/_config/routes.js
@@ -1,1 +1,9 @@
+// path: /app/_config/routes.js
+export const ROUTES = {
+  home: "/",
+  howItWorks: "/how-it-works",
+  start: "/start",
+  dashboard: "/case-dashboard",
+  preview: "/document-preview",
+};
 
