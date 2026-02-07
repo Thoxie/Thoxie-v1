fc8c025c90ccb7ce4d2e3a506d8afdb3a8a47b96
Thoxie <Paul@innitstreetwear.com>
2026-02-07 17:40:30 -0500

    Decrease header padding
    Reduced header padding from 5px to 4px.

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -7,7 +7,7 @@ export default function Header() {
     <header
       style={{
         backgroundColor: "#f15a22",
-        padding: "5px 15px",
+        padding: "4px 15px",
         display: "flex",
         alignItems: "center",
         justifyContent: "space-between",
