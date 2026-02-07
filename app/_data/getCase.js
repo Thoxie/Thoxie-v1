9e48c3dc7a7919152a105646077a4aa6bd294ac2
Thoxie <Paul@innitstreetwear.com>
2026-02-07 13:18:32 -0500

    Add getCaseFromQuery function for case retrieval

--- app/_data/getCase.js
+++ app/_data/getCase.js
@@ -1,1 +1,10 @@
+// path: /app/_data/getCase.js
+import { MOCK_CASE } from "./mockCase";
+import { MOCK_CASE_FILLED } from "./mockCaseFilled";
+
+export function getCaseFromQuery(searchParams) {
+  const sample = searchParams?.sample;
+  if (sample === "1" || sample === "true" || sample === "filled") return MOCK_CASE_FILLED;
+  return MOCK_CASE;
+}
 
