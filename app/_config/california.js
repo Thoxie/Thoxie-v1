5d2b52e532bb6a2fb2996f770125778ab10109bf
Thoxie <Paul@innitstreetwear.com>
2026-02-07 12:23:04 -0500

    Add California configuration settings

--- app/_config/california.js
+++ app/_config/california.js
@@ -1,1 +1,9 @@
+// path: /app/_config/california.js
+export const CALIFORNIA_CONFIG = {
+  state: "CA",
+  name: "California",
+  maxClaimAmount: 10000,
+  courts: "Small Claims",
+  enabled: true,
+};
 
