132bab9824cdea1482c29885b9b2a28f45ea65ec
Thoxie <Paul@innitstreetwear.com>
2026-02-07 13:49:29 -0500

    Create Container component for layout structure

--- app/_components/Container.js
+++ app/_components/Container.js
@@ -1,1 +1,16 @@
+// path: /app/_components/Container.js
+export default function Container({ children, style }) {
+  return (
+    <div
+      style={{
+        maxWidth: "980px",
+        margin: "0 auto",
+        padding: "24px 20px",
+        ...style,
+      }}
+    >
+      {children}
+    </div>
+  );
+}
 
