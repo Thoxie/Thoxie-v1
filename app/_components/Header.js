5f45047118d42e86d339ec1f0bdaf227c0d4fed0
Thoxie <Paul@innitstreetwear.com>
2026-02-07 17:06:52 -0500

    Increase logo height in Header component

--- app/_components/Header.js
+++ app/_components/Header.js
@@ -18,7 +18,7 @@ export default function Header() {
         <img
           src="/thoxie-logo.png"
           alt="Thoxie"
-          style={{ height: "72px" }}
+          style={{ height: "144px" }}
         />
         <StateBadge />
       </div>
