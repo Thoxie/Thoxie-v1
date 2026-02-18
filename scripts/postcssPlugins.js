59e3309da4ba3763077885f5e7d0b419823bfe24
Thoxie <Paul@innitstreetwear.com>
2026-02-18 10:27:30 -0500

    Add safeRequire and buildPlugins functions

--- scripts/postcssPlugins.js
+++ scripts/postcssPlugins.js
@@ -1,1 +1,23 @@
+// Path: /scripts/postcssPlugins.js
+function safeRequire(name) {
+  try {
+    // eslint-disable-next-line import/no-dynamic-require, global-require
+    return require(name);
+  } catch {
+    return null;
+  }
+}
+
+function buildPlugins() {
+  const plugins = {};
+
+  const autoprefixer = safeRequire("autoprefixer");
+  if (autoprefixer) {
+    plugins.autoprefixer = {};
+  }
+
+  return plugins;
+}
+
+module.exports = { buildPlugins };
 
